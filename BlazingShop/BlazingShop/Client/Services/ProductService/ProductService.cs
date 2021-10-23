8d837408ace605eed25533717c7f9f05c0e74df7
Siebe Holvoet <55406650+SHolvoet@users.noreply.github.com>
2021-10-23 14:50:58 +0200

    ep 1 finished

---
+++ BlazingShop/BlazingShop/Client/Services/ProductService/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using BlazingShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Client.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public void LoadProducts()
+        {
+            Products = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    Title = "The Hitchhiker's Guide to the Galaxy",
+                    Description = "The Hitchhiker's Guide to the Galaxy (sometimes referred to as HG2G, HHGTTG, H2G2, or tHGttG) is a comedy science fiction series created by Douglas Adams.",
+                    Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                    Price = 9.99m,
+                    OriginalPrice = 19.99m,
+                    DateCreated = new DateTime(2021, 1, 1)
+                },
+                new Product
+                {
+                    Id = 2,
+                    CategoryId = 3,
+                    Title = "Half-Life 2",
+                    Description = "Half-Life 2 is a 2004 first-person shooter game developed and published by Valve. Like the original Half-Life, it combines shooting, puzzles, and storytelling, and adds features such as vehicles and physics-based gameplay.",
+                    Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
+                    Price = 8.19m,
+                    OriginalPrice = 29.99m,
+                    DateCreated = new DateTime(2021, 1, 1)
+                }
+            };
+        }
+    }
+}
