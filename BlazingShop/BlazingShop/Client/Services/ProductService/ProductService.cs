37e8088336039f7d37f26cece34d294702053394
Siebe Holvoet <55406650+SHolvoet@users.noreply.github.com>
2021-10-24 11:23:55 +0200

    ep 2 finished

--- BlazingShop/BlazingShop/Client/Services/ProductService/ProductService.cs
+++ BlazingShop/BlazingShop/Client/Services/ProductService/ProductService.cs
@@ -2,41 +2,42 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace BlazingShop.Client.Services.ProductService
 {
     public class ProductService : IProductService
     {
+        private readonly HttpClient _http;
+
+        public event Action OnChange;
+
         public List<Product> Products { get; set; } = new List<Product>();
 
-        public void LoadProducts()
+        public ProductService(HttpClient http)
         {
-            Products = new List<Product>
+            _http = http;
+        }
+
+        public async Task LoadProducts(string categoryUrl = null)
+        {
+            if (categoryUrl == null)
             {
-                new Product
-                {
-                    Id = 1,
-                    CategoryId = 1,
-                    Title = "The Hitchhiker's Guide to the Galaxy",
-                    Description = "The Hitchhiker's Guide to the Galaxy (sometimes referred to as HG2G, HHGTTG, H2G2, or tHGttG) is a comedy science fiction series created by Douglas Adams.",
-                    Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
-                    Price = 9.99m,
-                    OriginalPrice = 19.99m,
-                    DateCreated = new DateTime(2021, 1, 1)
-                },
-                new Product
-                {
-                    Id = 2,
-                    CategoryId = 3,
-                    Title = "Half-Life 2",
-                    Description = "Half-Life 2 is a 2004 first-person shooter game developed and published by Valve. Like the original Half-Life, it combines shooting, puzzles, and storytelling, and adds features such as vehicles and physics-based gameplay.",
-                    Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
-                    Price = 8.19m,
-                    OriginalPrice = 29.99m,
-                    DateCreated = new DateTime(2021, 1, 1)
-                }
-            };
+                Products = await _http.GetFromJsonAsync<List<Product>>($"api/Product");
+            }
+            else
+            {
+                Products = await _http.GetFromJsonAsync<List<Product>>($"api/Product/Category/{categoryUrl}");
+            }
+            
+            OnChange.Invoke();
+        }
+
+        public async Task<Product> GetProduct(int id)
+        {
+            return await _http.GetFromJsonAsync<Product>($"api/Product/{id}");
         }
     }
 }
---
+++ BlazingShop/BlazingShop/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,63 @@
+﻿using BlazingShop.Server.Services.CategoryService;
+using BlazingShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Server.Services.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly ICategoryService _categoryService;
+
+        public ProductService(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public List<Product> Products { get; set; } = new List<Product>
+            {
+                new Product
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    Title = "The Hitchhiker's Guide to the Galaxy",
+                    Description = "The Hitchhiker's Guide to the Galaxy (sometimes referred to as HG2G, HHGTTG, H2G2, or tHGttG) is a comedy science fiction series created by Douglas Adams.",
+                    Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
+                    Price = 9.99m,
+                    OriginalPrice = 19.99m,
+                    DateCreated = new DateTime(2021, 1, 1)
+                },
+                new Product
+                {
+                    Id = 2,
+                    CategoryId = 3,
+                    Title = "Half-Life 2",
+                    Description = "Half-Life 2 is a 2004 first-person shooter game developed and published by Valve. Like the original Half-Life, it combines shooting, puzzles, and storytelling, and adds features such as vehicles and physics-based gameplay.",
+                    Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
+                    Price = 8.19m,
+                    OriginalPrice = 29.99m,
+                    DateCreated = new DateTime(2021, 1, 1)
+                }
+            };
+
+
+        public async Task<List<Product>> GetAllProducts()
+        {
+            return Products;
+        }
+
+        public async Task<Product> GetProduct(int id)
+        {
+            Product product = Products.FirstOrDefault(p => p.Id == id);
+            return product;
+        }
+
+        public async Task<List<Product>> GetProductsByCategory(string categoryUrl)
+        {
+            Category category = await _categoryService.GetCategoryByUrl(categoryUrl);
+            return Products.Where(p => p.CategoryId == category.Id).ToList();
+        }
+    }
+}
