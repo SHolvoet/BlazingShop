0b91462415b681c3185b06b4500dfc4f9ecd19d3
Siebe Holvoet <55406650+SHolvoet@users.noreply.github.com>
2021-10-25 21:03:47 +0200

    ep 5 finished

--- BlazingShop/BlazingShop/Server/Migrations/20211025184609_ProductVariants.Designer.cs
+++ BlazingShop/BlazingShop/Server/Migrations/20211025184609_ProductVariants.Designer.cs
@@ -4,14 +4,16 @@ using BlazingShop.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlazingShop.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20211025184609_ProductVariants")]
+    partial class ProductVariants
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -81,7 +83,7 @@ namespace BlazingShop.Server.Migrations
                         new
                         {
                             Id = 1,
-                            Name = "Default"
+                            Name = "Dafault"
                         },
                         new
                         {
---
+++ BlazingShop/BlazingShop/Server/Migrations/20211025184849_FixEditionTypo.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazingShop.Server.Migrations
+{
+    public partial class FixEditionTypo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Editions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Default");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Editions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Dafault");
+        }
+    }
+}
