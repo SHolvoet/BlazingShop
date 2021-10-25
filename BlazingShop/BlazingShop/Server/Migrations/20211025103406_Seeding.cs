cd09b957891125e7d6ad3767db48669d9358afe1
Siebe Holvoet <55406650+SHolvoet@users.noreply.github.com>
2021-10-25 12:49:41 +0200

    ep 3 finished

---
+++ BlazingShop/BlazingShop/Server/Migrations/20211025103406_Seeding.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazingShop.Server.Migrations
+{
+    public partial class Seeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 19.99m, 9.99m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 14.99m, 7.99m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 6.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 249.00m, 166.66m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 299m, 159.99m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 400m, 73.74m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 29.99m, 8.19m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 24.99m, 9.99m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Price",
+                value: 14.99m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 0m, 0m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 0m, 0m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 0m, 0m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 0m, 0m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 0m, 0m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 0m, 0m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "OriginalPrice", "Price" },
+                values: new object[] { 0m, 0m });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Price",
+                value: 0m);
+        }
+    }
+}
