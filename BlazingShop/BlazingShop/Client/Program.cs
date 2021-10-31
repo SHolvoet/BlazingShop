b371d73eb8e036b89cbb6c56f167772b87e896bb
Siebe Holvoet <55406650+SHolvoet@users.noreply.github.com>
2021-10-31 15:43:09 +0100

    ep 6 finished

--- BlazingShop/BlazingShop/Client/Program.cs
+++ BlazingShop/BlazingShop/Client/Program.cs
@@ -9,6 +9,9 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Blazored.Toast;
+using BlazingShop.Client.Services.CartService;
 
 namespace BlazingShop.Client
 {
@@ -22,6 +25,9 @@ namespace BlazingShop.Client
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<ICartService, CartService>();
+            builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddBlazoredToast();
 
             await builder.Build().RunAsync();
         }
